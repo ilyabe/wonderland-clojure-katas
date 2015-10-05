d77fe84e0751614c563c65adecd388b1d45e6d59
Ilya Bernshteyn <ilya.bernshteyn@guaranteedrate.com>
2015-10-05 10:05:30 -0500

    Notes and First Steps Wrote up a plan on how to handle encoding. Added a function to get the position of a letter in the alphabet, which will be handy in generating rows in the substitution chart.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,6 +1,24 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
+;; vector holding the alphabet
+;; fn to get the index of a letter
+;; fn to rotate alphabet letter's index times
+;; fn to get the encoded letter
+
+(def ^:private alphabet
+  ["a" "b" "c" "d" "e" "f"])
+
+(defn indx
+  "Function that returns the index of a letter `l` in `alphabet`."
+  [l]
+  (.indexOf alphabet l))
+
+(defn rotate
+  "Function to get the letter `l` row in the substitution chart."
+  [l])
+
+(defn encode
+  [keyword message]
   "encodeme")
 
 (defn decode [keyword message]
