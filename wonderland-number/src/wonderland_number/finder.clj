12c3b0055a02f95cb6c170f8ae96147ddca458c6
Ilya Bernshteyn <ilya.bernshteyn@guaranteedrate.com>
2015-11-19 11:52:36 -0600

    Find the magic number by checking every single six-digit integer.

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,17 @@
 (ns wonderland-number.finder)
 
+(defn brute []
+  "Brute force solution to produce a sequence of six-digit numbers
+  whose products of 2, 3, 4, 5, and 6 all contain the same digits
+  as the number itself."
+  (for [n (range 100000 1000000)
+        :let [n2 (set (str (* n 2)))
+              n3 (set (str (* n 3)))
+              n4 (set (str (* n 4)))
+              n5 (set (str (* n 5)))
+              n6 (set (str (* n 6)))]
+        :when (= (set (str n)) n2 n3 n4 n5 n6)]
+    n))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (first (brute)))
