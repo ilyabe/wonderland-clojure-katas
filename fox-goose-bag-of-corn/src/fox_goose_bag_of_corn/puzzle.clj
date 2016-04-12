9eae43d5ced1d1a122fc591c26e2e005e42191a1
Ilya Bernshteyn <ilya.bernshteyn@guaranteedrate.com>
2016-04-12 11:11:43 -0500

    The fox, goose, and corn are safely across

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,25 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
-(def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+(def start-pos [[:fox :goose :corn :you] [:boat] []])
 
 (defn river-crossing-plan []
-  start-pos)
+  "Fox can't be left with goose.
+  Goose can't be left with corn."
+  ; start
+  [start-pos
+  ; plan
+  [[:fox :corn] [:boat :goose :you] []]
+  [[:fox :corn] [:boat] [:you :goose]]
+  [[:fox :corn] [:boat :you] [:goose]]
+  [[:fox :corn :you] [:boat] [:goose]]
+  [[:fox] [:boat :you :corn] [:goose]]
+  [[:fox] [:boat] [:you :corn :goose]]
+  [[:fox] [:boat :you :goose] [:corn]]
+  [[:fox :you :goose] [:boat] [:corn]]
+  [[:goose] [:boat :you :fox] [:corn]]
+  [[:goose] [:boat] [:fox :you :corn]]
+  [[:goose] [:boat :you] [:fox :corn]]
+  [[:goose :you] [:boat] [:fox :corn]]
+  [[] [:boat :you :goose] [:fox :corn]]
+  ;end
+  [[] [:boat] [:fox :goose :corn :you]]])
